7076d2e8d07cca06c67794764889964fc74246f5
KuKKilicious <clouseyda@yahoo.de>
2017-07-13 17:40:46 +0200

    Animation on Sign when shot

--- JoCa1/Assets/DestroyAnimation.cs
+++ JoCa1/Assets/DestroyAnimation.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyAnimation : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+  
+    // Use this for initialization
+    void Start () {
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if (collision.tag.Equals("Projectile"))
+        {
+            Debug.Log("Projectile hit");
+            Animator a = GetComponent < Animator > ();
+            a.Play("destroy");
+        }
+    }
+
+   
+ 
+   
 }
